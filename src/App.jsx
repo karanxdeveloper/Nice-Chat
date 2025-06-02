4860353164ab69ec3151a8a17fd557782ea21d5b
karan <karan372003@gmail.com>
2025-06-02 23:05:14 +0530

    made the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from "./pages/LoginPage"
+import ProfilePage from "./pages/ProfilePage"
+// import bg from "./assets/"
 
 const App = () => {
   return (
-    <div>
-      <h1>chat application</h1>
+    <div className="bg-[url('./assets/BG_LOGO_BLACK.png')] bg-contain">
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+      </Routes>
     </div>
   )
 }
