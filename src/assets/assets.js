02cd3fa7fa90348ee4ad69e5f4280bc5263c57f8
karan <karan372003@gmail.com>
2025-06-04 22:50:09 +0530

    added the login and sign up page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,6 @@
 import avatar_icon from './avatar_icon.png'
 import gallery_icon from './gallery_icon.svg'
 import help_icon from './help_icon.png'
-import logo_icon from './logo_icon.svg'
-import logo_big from './logo_big.svg'
-import logo from './logo.png'
 import profile_richard from './profile_richard.png'
 import profile_alison from './profile_alison.png'
 import profile_enrique from './profile_enrique.png'
@@ -14,7 +11,6 @@ import send_button from './send_button.svg'
 import menu_icon from './menu_icon.png'
 import arrow_icon from './arrow_icon.png'
 import code from './code.svg'
-import bgImage from './bgImage.svg'
 import pic1 from './pic1.png'
 import pic2 from './pic2.png'
 import pic3 from './pic3.png'
@@ -27,15 +23,11 @@ const assets = {
     avatar_icon,
     gallery_icon,
     help_icon,
-    logo_big,
-    logo_icon,
-    logo,
     search_icon,
     send_button,
     menu_icon,
     arrow_icon,
     code,
-    bgImage,
     profile_martin,
     nice_chat_logo
 }
